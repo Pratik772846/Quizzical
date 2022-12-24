5bdd002026f1229da183c0297a3f827e63cb0934
Pratik <singhpratik015@gmail.com>
2022-12-24 12:51:34 +0530

    select only one option feature added

--- src/components/utility/format.js
+++ src/components/utility/format.js
@@ -6,23 +6,93 @@ export default function Format(props){
     const [active2,setActive2] = React.useState(false);
     const [active3,setActive3] = React.useState(false);
     const [active4,setActive4] = React.useState(false);
+    const [index,setIndex] = React.useState(0);
+
     function handleClick1(){
-        if(!active2 && !active3 && !active4)
+        if(active2 && !active3 && !active4){
+            setActive2(!active2);
             setActive1(!active1);
+            setIndex(1);
+        }
+        else if(!active2 && active3 && !active4){
+            setActive3(!active3);
+            setActive1(!active1);
+            setIndex(1);
+        }
+        else if(!active2 && !active3 && active4){
+            setActive4(!active4);
+            setActive1(!active1);
+            setIndex(1);
+        }
+        else if(!active2 && !active3 && !active4){
+            setActive1(!active1);
+            setIndex(1);
+        }
     }
     function handleClick2(){
-        if(!active1 && !active3 && !active4)
+        if(active1 && !active3 && !active4){
+            setActive1(!active1);
             setActive2(!active2);
+            setIndex(2);
+        }
+        else if(!active1 && active3 && !active4){
+            setActive3(!active3);
+            setActive2(!active2);
+            setIndex(2);
+        }
+        else if(!active1 && !active3 && active4){
+            setActive4(!active4);
+            setActive2(!active2);
+            setIndex(2);
+        }
+        else if(!active1 && !active3 && !active4){
+            setActive2(!active2);
+            setIndex(2);
+        }
       }
       function handleClick3(){
-        if(!active1 && !active2 && !active4)
+        if(active2 && !active1 && !active4){
+            setActive2(!active2);
             setActive3(!active3);
+            setIndex(3);
+        }
+        else if(!active2 && active1 && !active4){
+            setActive1(!active1);
+            setActive3(!active3);
+            setIndex(3);
+        }
+        else if(!active2 && !active1 && active4){
+            setActive4(!active4);
+            setActive3(!active3);
+            setIndex(3);
+        }
+        else if(!active2 && !active1 && !active4){
+            setActive3(!active3);
+            setIndex(3);
+        }
       }
       function handleClick4(){
-        if(!active1 && !active2 && !active3)
+        if(active2 && !active3 && !active1){
+            setActive2(!active2);
             setActive4(!active4);
+            setIndex(4);
+        }
+        else if(!active2 && active3 && !active1){
+            setActive3(!active3);
+            setActive4(!active4);
+            setIndex(4);
+        }
+        else if(!active2 && !active3 && active1){
+            setActive1(!active1);
+            setActive4(!active4);
+            setIndex(4);
+        }
+        else if(!active2 && !active3 && !active1){
+            setActive4(!active4);
+            setIndex(4);
+        }
       }
-
+      console.log(index);
     return(
         <div className="qanda">
           <h2 className="questions"><h1>{props.que}</h1> </h2>
